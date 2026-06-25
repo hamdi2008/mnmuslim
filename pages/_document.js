074d49820e2f71d5f6f43971e3746c1f963b9a26
Hamdi Mohamud <65649021+hamdi2008@users.noreply.github.com>
2026-06-25 14:19:27 -0500

    Add Poppins font & favicon to document

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <body>
         <Main />
