82f4742ba10969e25815be379649075cf7832385
Hamdi Mohamud <65649021+hamdi2008@users.noreply.github.com>
2026-06-25 14:41:36 -0500

    Fix: favicon using actual filename

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/ChatGPT Image Jun 25, 2026, 10_18_45 AM.png" />
       </Head>
       <body>
         <Main />
