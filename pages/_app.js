8aa6ce23421f5d48a0afe7158120f40a31033eb3
Hamdi Mohamud <65649021+hamdi2008@users.noreply.github.com>
2026-06-27 10:26:42 -0500

    feat: add Google Analytics GA4 (G-B1FN9KJHWC)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-B1FN9KJHWC"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-B1FN9KJHWC');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
