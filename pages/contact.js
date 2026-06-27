28477c5bc4891a5938833797d9e2890a4c74f417
Hamdi Mohamud <65649021+hamdi2008@users.noreply.github.com>
2026-06-27 07:54:55 -0500

    fix: remove Suggest a Feature, correct Instagram URL

--- pages/contact.js
+++ pages/contact.js
@@ -157,8 +157,7 @@ export default function Contact() {
                 <div className="hn-footer-col">
                   <div className="hn-footer-col-title">Company</div>
                   <Link href="/contact" className="hn-fl">Contact</Link>
-                  <Link href="/contact" className="hn-fl">Suggest a Feature</Link>
-                </div>
+                  </div>
                 <div className="hn-footer-col">
                   <div className="hn-footer-col-title">Resources</div>
                   <Link href="/submit" className="hn-fl">List Your Service</Link>
@@ -166,7 +165,7 @@ export default function Contact() {
                 </div>
                 <div className="hn-footer-col">
                   <div className="hn-footer-col-title">Community</div>
-                  <a href="https://instagram.com/mnmuslimevents" className="hn-fl" target="_blank" rel="noopener noreferrer">Instagram</a>
+                  <a href="https://www.instagram.com/mnmuslimevents/" className="hn-fl" target="_blank" rel="noopener noreferrer">Instagram</a>
                 </div>
               </div>
             </div>
